49bb03df22d301be41d23781b2f9deb45abeb91f
Jcartwright54 <jazmine.cartwright69@cuny.edu>
2026-09-15 16:55:34 +0000

    Added Task C

--- GIT/GIT.py
+++ GIT/GIT.py
@@ -3,10 +3,5 @@ print('Khalil Task A')
 print('Jazmine Task A')
 print('Melinda Task B')
 print('Khalil Task B')
-
-<<<<<<< HEAD
-print('Khalil Task C')
-=======
 print('Jazmine Task B')
-print('Jazmine Task C')
->>>>>>> 24e0cf7 (Added Task C)
+print('Khalil Task C')
